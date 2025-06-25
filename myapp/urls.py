cb9e1edc32aee535e2d7688b9259f58e0b651ada
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-25 14:27:06 +0530

    Comment out unused template extends and div in unauthorized_employees.html; clean up projects.html template

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('unauthorized_employees/', views.unauthorized_employees, name='unauthorized_employees'),
-    path('projects/', views.projects, name='projects'), 
+    path('unauthorized_employees/', views.unauthorized_employees, name='unauthorized_employees')
+    # path('projects/', views.projects, name='projects'), 
 ]
