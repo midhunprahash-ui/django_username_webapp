0e5dff55614b1c0294295f8e65513c6dc5b1d3bb
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-25 12:46:58 +0530

    Refactor MyappConfig class and app name for consistency

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig): # Renamed class
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp' # Renamed app name
+    name = 'myapp' 
